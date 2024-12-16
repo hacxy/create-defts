43bbaa24dbb85bc1775ec36463774dc06f2df579
hacxy <hacxy.js@outlook.com>
2024-12-16 10:10:53 +0800

    chore: remove log

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,15 @@ const __dirname = path.dirname(__filename);
 const templatePath = path.resolve(__dirname, '../templates');
 
 export async function bootstrap() {
-  console.log(templatePath);
   const projectName = await input({ message: 'input project name', default: 'ts-project', required: true }).catch(() => {
     console.log('已取消');
     process.exit(1);
   });
-  console.log(projectName);
+
   const template = await prompts(config);
-  console.log(template);
   const finalTempPath = path.resolve(templatePath, template);
-  console.log(finalTempPath);
   const targetPath = path.resolve(process.cwd(), projectName);
+
   cpSync(finalTempPath, targetPath, { recursive: true });
   renameSync(path.resolve(targetPath, '_gitignore'), path.resolve(targetPath, '.gitignore'));
 }
